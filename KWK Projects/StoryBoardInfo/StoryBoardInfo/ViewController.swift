dd39d38025ca5600b877e8ccfa296eaae420fdb4
Mia <51422536+miabryan@users.noreply.github.com>
2020-08-05 08:34:43 -0700

    Add files via upload

---
+++ KWK Projects/StoryBoardInfo/StoryBoardInfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoardInfo
+//
+//  Created by Apple on 8/5/20.
+//  Copyright © 2020 Mia Bryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   print("this is a tes")
+    }
+
+
+}
+
